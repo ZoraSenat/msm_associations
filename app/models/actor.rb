24ec6ceb852ca0fcb866dc3de79e373eb94b25f1
zora <ssh>
2016-11-15 16:23:00 -0600

    More corrections

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,4 +6,8 @@ class Actor < ActiveRecord::Base
   #  - dob: no rules
   #  - bio: no rules
   #  - image_url: no rules
+
+    has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
+
+    has_many(:movies, :class_name => "Movie", :foreign_key => "actor_id")
 end
