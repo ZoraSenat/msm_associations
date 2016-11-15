d83fc31088e9a38339a81cf184e2afeb527f600a
zora <ssh>
2016-11-15 15:48:22 -0600

    Added association helper method

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,6 @@
 class Character < ActiveRecord::Base
+
+
   # Character:
   #  - movie_id: must be present
   validates :name, :presence => true
@@ -7,4 +9,7 @@ class Character < ActiveRecord::Base
   validates :name, :presence => true
 
   #  - name: no rules
+    belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
+
+    belongs_to(:movie, :class_name => "Movie", :foreign_key => "movie_id")
 end
